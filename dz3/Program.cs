bea91fb133b0638c8eda569bfcc6fe3ab8eb1911
Alim-Almukhametov <Swain221221@mail.ru>
2024-04-08 01:10:21 +0500

    dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] mass = { 1, 2, 0, 54, 15, 7, };
+
+void MassPrint(int[] mass, int i = 0)
+{
+    if (i == mass.Length)
+    {
+        return;
+    }
+
+    MassPrint(mass, i + 1);
+    Console.Write($"{mass[i]} ");
+}
+
+MassPrint(mass);
