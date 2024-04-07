c01d81d9202aed8ec708c5458c397e09bee58dec
Alim-Almukhametov <Swain221221@mail.ru>
2024-04-08 00:42:03 +0500

    dz1

---
+++ dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿string NaturNumb(int m, int n)
+{
+    if(m == n) return Convert.ToString(n);
+    return m + " " + NaturNumb(m + 1, n);
+}
+
+Console.WriteLine("Введите число: M ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write(NaturNumb(m, n));
+
+
+
