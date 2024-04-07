9228511964c57b89843b92246d58ef93c65169d0
Alim-Almukhametov <Swain221221@mail.ru>
2024-04-08 01:11:02 +0500

    dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int AkirmanFunk(int n, int m)
+{
+    if(n == 0){
+        return m + 1;
+    }
+
+    if(n > 0 && m == 0)
+    {
+        return AkirmanFunk(n - 1, m = 1);
+    }
+    if(n > 0 && m > 0)
+    {
+        return AkirmanFunk(n - 1, AkirmanFunk(n ,m - 1));
+    }
+    return AkirmanFunk(n, m);
+}
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write(AkirmanFunk(n, m ));
+
